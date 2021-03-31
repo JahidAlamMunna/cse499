ab7dd6ea7a9bc86ade662d7ee1de5634b505a6d5
Jahid Alam Munna <jahid.munna@northsouth.edu>
2021-03-31 20:25:09 +0600

    doctor updated

--- PROJECT-CODE/database/migrations/2021_03_30_121727_create_doctors_table.php
+++ PROJECT-CODE/database/migrations/2021_03_30_121727_create_doctors_table.php
@@ -14,8 +14,18 @@ class CreateDoctorsTable extends Migration
     public function up()
     {
         Schema::create('doctors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+    $table->bigIncrements('doc_id');
+    $table->string('doc_name');
+    $table->string('doc_post');
+    $table->string('doc_division');
+    $table->string('doc_address');
+    $table->string('doc_degree');
+    $table->string('doc_spec'); //doctor's spceicialty,
+    $table->string('doc_hospital'); // the doctor's chamber
+    $table->string('doc_contact'); //contact number
+    $table->timestamps();
+        
+        
         });
     }
 
